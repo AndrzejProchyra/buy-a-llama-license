50101172dab7ea77e30746f06c1f610fea161278
Andrzej Prochyra <andrzej.prochyra@digital.cabinet-office.gov.uk>
2025-12-01 09:38:37 +0000

    initial commit

---
+++ src/main/java/org/example/buyallamalicense/BuyALlamaLicenseApplication.java
@@ -0,0 +1,13 @@
+package org.example.buyallamalicense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyALlamaLicenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyALlamaLicenseApplication.class, args);
+    }
+
+}
