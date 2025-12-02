14a4bd15c35b32d84481a16f09566a6522287d37
Andrzej Prochyra <andrzej.prochyra@digital.cabinet-office.gov.uk>
2025-12-02 14:34:09 +0000

    add test for id assignment and uniqueness

--- src/test/java/org/example/buyallamalicense/adapters/InMemoryPaymentRepositoryTest.java
+++ src/test/java/org/example/buyallamalicense/adapters/InMemoryPaymentRepositoryTest.java
@@ -20,12 +20,30 @@ class InMemoryPaymentRepositoryTest {
 
     @Test
     void should_save_and_find_a_payment() {
-        PaymentId paymentId = new PaymentId(42);
+        var paymentId = new PaymentId(42);
         var payment = new Payment(paymentId, new ExternalPaymentId());
 
         var id = paymentRepository.save(payment);
 
         then(paymentRepository.findById(id))
                 .isEqualTo(payment);
+    }
+
+    @Test
+    void should_assign_a_unique_id_to_new_payments() {
+        var payment1 = new Payment(new ExternalPaymentId());
+        var payment2 = new Payment(new ExternalPaymentId());
+        var payment3 = new Payment(new ExternalPaymentId());
+
+        var id1 = paymentRepository.save(payment1);
+        var id2 = paymentRepository.save(payment2);
+        var id3 = paymentRepository.save(payment3);
+        var savedPayment1 = paymentRepository.findById(id1);
+        var savedPayment2 = paymentRepository.findById(id2);
+        var savedPayment3 = paymentRepository.findById(id3);
+
+        then(savedPayment1.id()).isNotNull()
+                .isNotEqualTo(savedPayment2.id())
+                .isNotEqualTo(savedPayment3.id());
     }
 }
